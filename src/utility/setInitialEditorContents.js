c4936652c224be2182037c3fbb9bc691f438f882
Erica DiGiulio <digiulioem@gmail.com>
2022-02-02 02:00:05 -0700

    Add space before code block in initial code

--- src/utility/setInitialEditorContents.js
+++ src/utility/setInitialEditorContents.js
@@ -7,7 +7,7 @@ module.exports = function (editor, exerciseName, exercise) {
         editor.setValue(localStorage.getItem(exerciseCode));
     } else {
         // no previous solution, just display the default...
-        editor.setValue('function ' + exercise.name + '(' + defaultInput(exercise.name) + '){\n  \n}');
+        editor.setValue('function ' + exercise.name + '(' + defaultInput(exercise.name) + ') {\n  \n}');
         editor.focus();
         editor.setCursor({
             line: 1,
